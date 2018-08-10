8d8e408433251840538e7ed90dde1a34bd0bd72d
Kyle-Neale <kyle.a.neale@gmail.com>
2018-08-10 17:38:30 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,30 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student = Student.new(student_hash)
+    if student.save
+      redirect_to student_path(student)
+    else
+      redirect_to new_student_path(student)
+    end
+  end
+
+  private
+
+  def student_hash
+    hash_student = params.require(:student).permit(:first_name, :last_name)
   end
 
 end
